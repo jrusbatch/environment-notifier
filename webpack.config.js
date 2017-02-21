a8637d3ac98da350d66c7ad6aca37bc719b4191a
Ken Dale <ken@kendaleiv.com>
2017-02-21 16:55:27 -0500

    Run HtmlWebpackPlugin for webpack-dev-server only

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,25 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('./package');
 
+const plugins = [
+  new webpack.BannerPlugin(
+`Environment Notifier v${packageJson.version}
+Copyright (c) 2017 Ritter Insurance Marketing`),
+  new webpack.optimize.UglifyJsPlugin({
+    sourceMap: true
+  })
+];
+
+if (process.env.INCLUDE_WEBPACK_HTML) {
+  plugins.push(
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      filename: '../index.html',
+      inject: 'head'
+    })
+  );
+}
+
 module.exports = {
   entry: [
     './src/environment-notifier.js'
@@ -22,17 +41,5 @@ module.exports = {
     ]
   },
   devtool: 'source-map',
-  plugins: [
-    new webpack.BannerPlugin(
-`Environment Notifier v${packageJson.version}
-Copyright (c) 2017 Ritter Insurance Marketing`),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: '../index.html',
-      inject: 'head'
-    })
-  ]
+  plugins: plugins
 };
